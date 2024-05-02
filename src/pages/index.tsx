ab66283f772515d17599f0f343f56865ec8fdfbc
muhammadwaqarqsol <muwaqar.qsol@gmail.com>
2024-05-02 17:05:15 +0500

    svg added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -47,7 +48,40 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <Link href={"https:www.google.com"} target="_blank">
+              <p className="text-gray-600">Cool Car</p>
+            </Link>
+          </div>
+          <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              width="32"
+              height="32"
+
+            >
+              <path
+                fill="#1877f2"
+                d="M160 8C74.99 8 8 74.99 8 160v192c0 85.98 66.99 152 152 152s152-66.98 152-152V160c0-85.01-68.98-152-152-152zm52.1 160h-28.63c-3.68 0-6.64 2.97-6.64 6.65v34.22h35.27c1.67 0 3.03 1.36 3.03 3.03v41.88c0 1.68-1.36 3.04-3.03 3.04h-35.27v105.12c0 1.68-1.36 3.03-3.04 3.03h-47.52c-1.67 0-3.03-1.36-3.03-3.03V356.8h-19.14c-1.67 0-3.03-1.36-3.03-3.04V312.8c0-1.67 1.36-3.03 3.03-3.03h19.14v-25.44c0-16.72 3.99-34.41 12.08-48.96 8.25-14.82 20.52-26.31 35.91-34.56 15.4-8.25 32.53-12.37 50.75-12.37h28.62c1.67 0 3.03 1.35 3.03 3.03v34.21h-19.14z"
+              />
+            </svg>
+{/* 
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              stroke="white"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="h-6 w-6 flex-none text-violet-600"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <path d="m9 12 2 2 4-4" />
+            </svg> */}
+            <p className="text-gray-600">Cool Bike</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -65,7 +99,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">Cool Term</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -83,7 +117,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">Cool DELL</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -101,7 +135,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">Cool check</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -119,7 +153,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">TERM SHEET</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -137,7 +171,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">Street Fighter</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -155,7 +189,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">Cool FB</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -173,7 +207,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">Cool SOFTWARE</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -191,7 +225,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">Cool DEV</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -209,7 +243,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">ALANWAKE2</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -227,7 +261,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">Justice Society</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -245,7 +279,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">House of Dragon</p>
           </div>
           <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
             <svg
@@ -263,25 +297,7 @@ export default function Home() {
               <circle cx="12" cy="12" r="10" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            <p className="text-gray-600">Cool Stroke</p>
-          </div>
-          <div className="flex cursor-pointer items-center space-x-2 rounded-md border border-gray-100 p-5 shadow-md transition-all hover:-translate-y-1 hover:translate-x-1 hover:scale-[1.025] hover:shadow-xl">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              stroke="white"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="h-6 w-6 flex-none text-violet-600"
-            >
-              <circle cx="12" cy="12" r="10" />
-              <path d="m9 12 2 2 4-4" />
-            </svg>
-            <p className="text-gray-600">Cool Stroke</p>
+            <p className="text-gray-600">Just Chill</p>
           </div>
         </div>
       </div>
